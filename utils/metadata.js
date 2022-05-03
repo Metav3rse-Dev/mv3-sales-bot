f216f3f80eca188c00ac093925acf32ef4b33fb8
Flamenate <haythem.titou@live.fr>
2022-05-03 19:09:25 +0100

    use ternary operators & unpack syntax

--- utils/metadata.js
+++ utils/metadata.js
@@ -1,22 +1,22 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const getIPFSURL = (uri) => uri.split('://');
+const getIPFSURL = (uri) => uri.split("://");
 
 const fetchMetadata = async (uri) => {
-  const urlParts = getIPFSURL(uri);
-  const prefix = urlParts[0];
-  const ipfsURL = urlParts[1];
-  console.log({ prefix, ipfsURL });
-  let metadata = '';
-  if (prefix === 'ipfs') metadata = await axios.get(`https://ipfs.io/ipfs/${ipfsURL}`);
-  else metadata = await axios.get(uri);
-  return {
-    name: metadata.data.name,
-    image:
-      prefix === 'ipfs'
-        ? `https://ipfs.io/ipfs/${getIPFSURL(metadata.data.image)[1]}`
-        : metadata.data.image,
-  };
+	const urlParts = getIPFSURL(uri);
+	const [prefix, ipfsURL] = urlParts;
+	console.log({ prefix, ipfsURL });
+	let metadata;
+	metadata = await axios.get(
+		prefix === "ipfs" ? `https://ipfs.io/ipfs/${ipfsURL}` : uri,
+	);
+	return {
+		name: metadata.data.name,
+		image:
+			prefix === "ipfs"
+				? `https://ipfs.io/ipfs/${getIPFSURL(metadata.data.image)[1]}`
+				: metadata.data.image,
+	};
 };
 
 module.exports = { fetchMetadata };
