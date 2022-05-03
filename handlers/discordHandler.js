4eb6ea5926e90dfd7d253cb726223e3992f4f6f1
Flamenate <haythem.titou@live.fr>
2022-05-03 19:07:50 +0100

    remove unnecessary variables

--- handlers/discordHandler.js
+++ handlers/discordHandler.js
@@ -1,33 +1,30 @@
-require('dotenv');
-const { discordSetup, createMessage } = require('../utils/discord');
-
-// Environment file used to host API keys
-require('dotenv').config();
+const { discordSetup, createMessage } = require("../utils/discord");
 
 const sendDiscordMessage = async (message) => {
-  try {
-    const channel = await discordSetup(
-      process.env.DISCORD_BOT_TOKEN,
-      process.env.DISCORD_CHANNEL_ID,
-    );
+	try {
+		const channel = await discordSetup(
+			process.env.DISCORD_BOT_TOKEN,
+			process.env.DISCORD_CHANNEL_ID,
+		);
 
-    const discordMessage = createMessage(
-      message.name,
-      message.image,
-      message.value,
-      message.to,
-      message.from,
-      message.timestamp,
-      message.tokenId,
-    );
-    const returnMessage = await discordMessage;
-    return channel.send({ embeds: [returnMessage] });
-  } catch (e) {
-    console.error(e);
-    return 'An error occurred.';
-  }
+		return await channel.send({
+			embeds: [
+				await createMessage(
+					message.name,
+					message.image,
+					message.value,
+					message.to,
+					message.from,
+					message.timestamp,
+					message.tokenId,
+				),
+			],
+		});
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 module.exports = {
-  sendDiscordMessage,
+	sendDiscordMessage,
 };
