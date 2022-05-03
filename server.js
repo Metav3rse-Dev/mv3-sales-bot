fa6fe1108cd0fc9648445f8dd533919beccef7d1
Flamenate <haythem.titou@live.fr>
2022-05-03 19:02:43 +0100

    call dotenv config once only

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const bodyParser = require('body-parser');
-const { getEvents } = require('./getEvents');
+const bodyParser = require("body-parser");
+const { getEvents } = require("./getEvents");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(process.env.PORT || 8000, () => {
-  getEvents();
+	getEvents();
 });
